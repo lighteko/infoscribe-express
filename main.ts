3a3581d4c04eec6d394be9c016f068cb32575afe
lighteko <dev.lighteko@gmail.com>
2025-03-21 19:48:15 -0400

    EDIT: Hide port

--- main.ts
+++ main.ts
@@ -3,9 +3,10 @@ import createApp from "@src/app";
 const app = createApp();
 
 async function main() {
-  const PORT = 8000;
-  app.listen(`${PORT}`, () => {
-    console.log(`\n* Server running on http://127.0.0.1:${PORT}\n`);
+  const EXPRESS_PORT = process.env.EXPRESS_PORT as any as number;
+
+  app.listen(EXPRESS_PORT, '0.0.0.0', () => {
+    console.log(`\n* Server running on http://127.0.0.1:${EXPRESS_PORT}\n`);
   });
 }
 
